01b92f0ec9afddf926acabab8b85539001e45d74
piano <maryesmaeely@gmail.com>
2020-11-17 10:39:49 +0100

    some more stuff added

--- src/Model/users/User.java
+++ src/Model/users/User.java
@@ -1,4 +1,6 @@
 package Model.users;
+import java.util.UUID;
+
 
 public class User {
 
@@ -6,15 +8,44 @@ public class User {
      * Attributes
      */
 
+    private String id = UUID.randomUUID().toString();
+    private String userName;
+    private String password;
+
     /**
      * Constructor
      */
+
+    public User(String userName, String passord) {
+        this.userName = userName;
+        this.password = password;
+    }
 
     /**
      * Getters and Setters
      */
 
+    public String getUserName() {
+        return userName;
+    }
+
+    public void setUserName(String userName) {
+        this.userName = userName;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
     /**
      * Methods
      */
+    public String toString() {
+        return "Your username is:\n" + this.userName + "\nYour password is:\n" + this.password + "\nDon't forget these "
+                + "information or you will be cursed to not be able to use this app forever";
+    }
 }
