700f2b14c948318458b02a65a6d071a06d0f3b61
Maryame <maryame@student.chalmers.se>
2021-01-03 20:20:12 +0100

    not done yet

--- src/utilities/InputErrors.java
+++ src/utilities/InputErrors.java
@@ -4,28 +4,53 @@ public class InputErrors {
 
     public static String emptyFieldString(String userInput) {
         while (userInput.equals("")) {
-            userInput = InputOutput.inputString("you did not insert any information for this field. Please try one more time");
+            System.err.println("\nYou did not insert any information for this field.");
+            userInput = InputOutput.inputString("Please try one more time");
         }
         return userInput;
     }
 
-//    public static int emptyFieldInt(Integer userInput) {
-//        while (userInput.equals("")) {
-//            userInput = InputOutput.inputInt("you did not insert any information for this field. Please try one more time");
-//        }
-//        return userInput;
-//    }
+    public static int irrelevantInt(String userInput) {
+        int correctInput = 0;
+        try {
+            correctInput = Integer.parseInt(userInput);
+        }catch(NumberFormatException wrongInput){
+            System.err.println("\nYou should insert a number here.");
+            userInput = InputOutput.inputString("Please try again");
+            irrelevantInt(userInput);
+
+        }
+        return correctInput;
+    }
 
     public static double irrelevantDouble(String userInput) {
-        double sth = 0.0;
+        double correctInput = 0.0;
         try {
-            sth = Integer.parseInt(userInput);
-        }catch(NumberFormatException ex){
-            System.err.println("You should insert a number here.");
+            correctInput = Double.parseDouble(userInput);
+        }catch(NumberFormatException wrongInput){
+            System.err.println("\nYou should insert a number here.");
             userInput = InputOutput.inputString("Please try again");
-            sth = Integer.parseInt(userInput);
+            irrelevantDouble(userInput);
+
         }
-        return sth;
+        return correctInput;
+    }
+
+    public static String incorrectYesOrNo (String userInput) {
+        userInput = InputOutput.inputString("Invalid input. you should answer with yes " +
+                "or no. Please try again");
+
+        switch (userInput) {
+            case "yes" -> {
+                return "yes";
+            }
+            case "no" -> {
+                return "no";
+            }
+            default -> {
+                return incorrectYesOrNo(userInput);
+            }
+        }
     }
 
 }
